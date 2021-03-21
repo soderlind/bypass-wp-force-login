16bd3e558dcb014d32d4e8f1045aba2f852dd387
Per Søderlind <per@soderlind.no>
2021-03-21 01:31:50 +0100

    Refactor

--- src/Tools/Helper.php
+++ src/Tools/Helper.php
@@ -27,25 +27,25 @@ class Helper {
 
 		if ( ! is_plugin_active( 'wp-force-login/wp-force-login.php' ) ) {
 			set_transient( 'bypass-wp-force-login-deactivate', true, 5 );
-		}
 
-		add_action(
-			'admin_notices',
-			function () : void {
-				if ( get_transient( 'bypass-wp-force-login-deactivate' ) ) {
+			add_action(
+				'admin_notices',
+				function () : void {
+					if ( get_transient( 'bypass-wp-force-login-deactivate' ) ) {
 
-					$msg[] = '<div class="notice notice-error is-dismissible ">';
-					$msg[] = '<p><strong>Bypass Force Login</strong></p>';
-					$msg[] = '<p>Please install and activate <a href="https://wordpress.org/plugins/wp-force-login/">Force Login</a> before you activate Bypass Force Login</p>';
-					$msg[] = '</div>';
-					echo implode( PHP_EOL, $msg );
+						$msg[] = '<div class="notice notice-error is-dismissible ">';
+						$msg[] = '<p><strong>Bypass Force Login</strong></p>';
+						$msg[] = '<p>Please install and activate <a href="https://wordpress.org/plugins/wp-force-login/">Force Login</a> before you activate Bypass Force Login</p>';
+						$msg[] = '</div>';
+						echo implode( PHP_EOL, $msg );
 
-					delete_transient( 'bypass-wp-force-login-activate' );
-					deactivate_plugins( BYPASS_BASENAME );
+						delete_transient( 'bypass-wp-force-login-activate' );
+						deactivate_plugins( BYPASS_BASENAME );
 
+					}
 				}
-			}
-		);
+			);
+		}
 	}
 
 	/**
