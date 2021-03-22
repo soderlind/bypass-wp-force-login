e08dbae2adf0d87a87e20609b3b1f7ad1573774c
Per Søderlind <per@soderlind.no>
2021-03-22 21:23:11 +0100

    1.0.10

--- bypass-wp-force-login.php
+++ bypass-wp-force-login.php
@@ -12,7 +12,7 @@
  * Plugin URI: https://github.com/soderlind/bypass-wp-force-login
  * GitHub Plugin URI: https://github.com/soderlind/bypass-wp-force-login
  * Description: Easily add exceptions to <a href="https://wordpress.org/plugins/wp-force-login/">Force Login</a>.
- * Version:     1.0.9
+ * Version:     1.0.10
  * Author:      Per Soderlind
  * Author URI:  https://soderlind.no
  * Text Domain: bypass-wp-force-login
